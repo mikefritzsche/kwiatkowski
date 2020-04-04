a3590aece212db0727689015628536e70cd6256c
Mike Fritzsche <mike@mikefritzsche.com>
2020-04-04 13:59:20 -0700

    add missed files from initial

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+import routesData from './static/routesData.json'
+
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/kwiatkowski/'
+  }
+} : {}
+
+export default {
+  ...routerBase,
+  generate: {
+    minnify: false,
+    routes() {
+      const exportRoutesData = routesData.map(item => {
+        return {route: `/${item.section}/${item.name}`, payload: item}
+        })
+      return Promise.resolve(exportRoutesData)
+    }
+  },
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  },
+  server: {
+    port: 8000, // default: 3000
+    // host: '0.0.0.0' // default: localhost
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <img class="name-sticker left" src="../static/name7.jpg" alt="">
+    <img class="name-sticker right" src="../static/name7.jpg" alt="">
+    <div>
+      <h1 class="title">
+        <nuxt-link to="/art">The Art</nuxt-link>
+        <nuxt-link to="/artist">The Artist</nuxt-link>
+      </h1>
+      <h2 class="subtitle">
+        The art of Stan Kwiatkowski
+      </h2>
+      <div class="links">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  head() {
+    return {
+      title: `The Art of Kwiatkowski`,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: "description", name: "description", content: 'The art of Kwiatkowski' }
+      ],
+      bodyAttrs: {
+        class: "home page"
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  body {
+
+  }
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  /*display: flex;*/
+  background: url(../static/dark_self4.jpg) black no-repeat center fixed;
+
+  h1 {
+    &.title {
+      font-size: 2rem;
+      a {
+        color: #996633;
+        text-decoration: none;
+        &:first-child {
+
+        }
+        &:last-child {
+          position: relative;
+          right: 0;
+        }
+      }
+    }
+  }
+  .name-sticker {
+    position: fixed;
+    &.left {
+      left: 0
+    }
+    &.right {
+      right: 0;
+    }
+  }
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
